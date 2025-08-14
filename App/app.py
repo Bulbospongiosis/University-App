7b02505a4ad85c3700250437a2e4ad34064171e0
William Huang <williamhuang9611@gmail.com>
2025-08-14 11:24:37 -0400

    v

--- App/app.py
+++ App/app.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/form/question1", methods=['GET', 'POST'])
-def form():
+@app.route('/form/question1', methods=['GET', 'POST'])
+def form1():
     return render_template("form.html")
 
-@app.route("/form/question2", methods=['GET', 'POST'])
-def form():
+@app.route('/form/question2', methods=['GET', 'POST'])
+def form2():
     return render_template("form2.html")
 
 @app.route("/form/question3", methods=['GET', 'POST'])
-def form():
+def form3():
     return render_template("form3.html")
 
 @app.route("/form/question4", methods=['GET', 'POST'])
-def form():
+def form4():
     return render_template("form4.html")
 
 
